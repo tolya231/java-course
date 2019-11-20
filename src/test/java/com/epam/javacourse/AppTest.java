7753faba2cd665bf26febcc64c501d2a0e30ca13
Shelikhovskii_Anatolii <ashelikhovskii.ext@diamos.com>
2019-11-20 14:13:01 +0300

    test created

---
+++ src/test/java/com/epam/javacourse/AppTest.java
@@ -0,0 +1,13 @@
+package com.epam.javacourse;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppTest
+{
+  @Test
+  public void testTrue() {
+    assertTrue(true);
+  }
+}
