4d790512e896c17dcfc7d133f07e4eacbce51f52
Shelikhovskii_Anatolii <ashelikhovskii.ext@diamos.com>
2019-12-11 15:09:48 +0300

    [web-apps] removed embedded tomcat. Prepared class for load testing

--- servletsapp/src/main/java/servlets/AttributeServlet.java
+++ servletsapp/src/main/java/servlets/AttributeServlet.java
@@ -2,7 +2,6 @@ package servlets;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import java.io.PrintWriter;
 public class AttributeServlet extends HttpServlet
 {
   @Override
-  protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
   {
     ServletContext ctx = req.getServletContext();
     ctx.setAttribute( "user", "Anatolii" );
--- servletsapp/src/main/java/servlets/MainServlet.java
+++ servletsapp/src/main/java/servlets/MainServlet.java
@@ -1,19 +1,19 @@
 package servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    PrintWriter writer = resp.getWriter();
-    writer.print("<h1>Hello world!<h1>");
-    writer.close();
-  }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>Hello world!<h1>");
+        writer.close();
+    }
 }
