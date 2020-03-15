6cc3289fd0759e4f9648cee704610fc599af4da0
Anatoly Shelikhovsky <anatolii_shelikhovskii@epam.com>
2020-03-15 17:05:15 +0300

    transactions logic moved to TransactionalDogService

--- dogwebapp/src/main/java/com/epam/services/DogService.java
+++ dogwebapp/src/main/java/com/epam/services/DogService.java
@@ -1,81 +1,29 @@
 package com.epam.services;
 
-import static com.epam.jdbc.JdbcConnectionHolder.closeConnection;
-import static com.epam.jdbc.JdbcConnectionHolder.rollbackConnection;
-
 import com.epam.dto.DogDto;
-import com.epam.jdbc.JdbcConnectionHolder;
 import com.epam.repositories.DogDao;
-import java.sql.Connection;
 
 public class DogService implements CommonCrudService {
 
   private final DogDao dogDao;
-  private final JdbcConnectionHolder jdbcConnectionHolder;
 
-
-  public DogService(DogDao dogDao, JdbcConnectionHolder jdbcConnectionHolder) {
+  public DogService(DogDao dogDao) {
     this.dogDao = dogDao;
-    this.jdbcConnectionHolder = jdbcConnectionHolder;
   }
 
   public DogDto create(DogDto dog) {
-    Connection connection = null;
-    try {
-      connection = jdbcConnectionHolder.createConnection();
-      connection.setAutoCommit(false);
-
-      return dogDao.create(dog);
-    } catch (Exception e) {
-      rollbackConnection(connection);
-      throw new RuntimeException(String.format("Create dog %s failed", dog), e);
-    } finally {
-      closeConnection(connection);
-    }
+    return dogDao.create(dog);
   }
 
   public DogDto update(DogDto dog) {
-    Connection connection = null;
-    try {
-      connection = jdbcConnectionHolder.createConnection();
-      connection.setAutoCommit(false);
-
-      return dogDao.update(dog);
-    } catch (Exception e) {
-      rollbackConnection(connection);
-      throw new RuntimeException(String.format("Update dog %s failed", dog), e);
-    } finally {
-      closeConnection(connection);
-    }
+    return dogDao.update(dog);
   }
 
   public DogDto get(long id) {
-    Connection connection = null;
-    try {
-      connection = jdbcConnectionHolder.createConnection();
-      connection.setAutoCommit(false);
-
-      return dogDao.get(id);
-    } catch (Exception e) {
-      rollbackConnection(connection);
-      throw new RuntimeException(String.format("Get dog with id=%s failed", id), e);
-    } finally {
-      closeConnection(connection);
-    }
+    return dogDao.get(id);
   }
 
   public void delete(long id) {
-    Connection connection = null;
-    try {
-      connection = jdbcConnectionHolder.createConnection();
-      connection.setAutoCommit(false);
-
-      dogDao.delete(id);
-    } catch (Exception e) {
-      rollbackConnection(connection);
-      throw new RuntimeException(String.format("Delete dog with id=%s failed", id), e);
-    } finally {
-      closeConnection(connection);
-    }
+    dogDao.delete(id);
   }
 }
--- dogwebapp/src/main/java/com/epam/services/TransactionalDogService.java
+++ dogwebapp/src/main/java/com/epam/services/TransactionalDogService.java
@@ -1,33 +1,85 @@
 package com.epam.services;
 
+import static com.epam.jdbc.JdbcConnectionHolder.closeConnection;
+import static com.epam.jdbc.JdbcConnectionHolder.rollbackConnection;
+
 import com.epam.dto.DogDto;
+import com.epam.jdbc.JdbcConnectionHolder;
+import java.sql.Connection;
 
 public class TransactionalDogService implements CommonCrudService {
 
   private final CommonCrudService dogService;
+  private final JdbcConnectionHolder jdbcConnectionHolder;
 
-  public TransactionalDogService(CommonCrudService dogService) {
+  public TransactionalDogService(CommonCrudService dogService,
+      JdbcConnectionHolder jdbcConnectionHolder) {
     this.dogService = dogService;
+    this.jdbcConnectionHolder = jdbcConnectionHolder;
   }
 
   @Override
   public DogDto create(DogDto dog) {
-    return dogService.create(dog);
+    Connection connection = null;
+    try {
+      connection = jdbcConnectionHolder.createConnection();
+      connection.setAutoCommit(false);
+
+      return dogService.create(dog);
+    } catch (Exception e) {
+      rollbackConnection(connection);
+      throw new RuntimeException(String.format("Create dog %s failed", dog), e);
+    } finally {
+      closeConnection(connection);
+    }
   }
 
   @Override
   public DogDto update(DogDto dog) {
-    return dogService.update(dog);
+    Connection connection = null;
+    try {
+      connection = jdbcConnectionHolder.createConnection();
+      connection.setAutoCommit(false);
+
+      return dogService.update(dog);
+    } catch (Exception e) {
+      rollbackConnection(connection);
+      throw new RuntimeException(String.format("Update dog %s failed", dog), e);
+    } finally {
+      closeConnection(connection);
+    }
   }
 
   @Override
   public DogDto get(long id) {
-    return dogService.get(id);
+    Connection connection = null;
+    try {
+      connection = jdbcConnectionHolder.createConnection();
+      connection.setAutoCommit(false);
+
+      return dogService.get(id);
+    } catch (Exception e) {
+      rollbackConnection(connection);
+      throw new RuntimeException(String.format("Get dog with id=%s failed", id), e);
+    } finally {
+      closeConnection(connection);
+    }
   }
 
   @Override
   public void delete(long id) {
-    dogService.delete(id);
+    Connection connection = null;
+    try {
+      connection = jdbcConnectionHolder.createConnection();
+      connection.setAutoCommit(false);
+
+      dogService.delete(id);
+    } catch (Exception e) {
+      rollbackConnection(connection);
+      throw new RuntimeException(String.format("Delete dog with id=%s failed", id), e);
+    } finally {
+      closeConnection(connection);
+    }
   }
 
 }
